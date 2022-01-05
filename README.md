e7763a730f74bf3936003da6327fa01bbc07463b
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2022-01-05 12:34:21 +0100

    Improvements

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Typescript Toolbox
 
+[![pipeline status](https://gitlab.com/ludo237/ts-toolbox/badges/main/pipeline.svg)](https://gitlab.com/ludo237/ts-toolbox/-/commits/main)
+[![coverage report](https://gitlab.com/ludo237/ts-toolbox/badges/main/coverage.svg)](https://gitlab.com/ludo237/ts-toolbox/-/commits/main)
+
 A collection of useful agnostic functions that I've created trough time. They can be useful anywhere
 
 ## To Do
--- jest.config.ts
+++ jest.config.ts
@@ -20,26 +20,21 @@ export default {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ["src/**/*.ts", "!**/node_modules/**"],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "\\\\node_modules\\\\"
-  // ],
+  coveragePathIgnorePatterns: [
+    "\\\\node_modules\\\\"
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: ["json", "html", "text", "text-summary", "cobertura"],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
@@ -146,10 +141,10 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
